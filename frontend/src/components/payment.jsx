364c79dba7015836879055ee0565380382d92066
Sarang <sarang.p1230@gmail.com>
2026-04-04 23:59:07 +0530

    set payment frontend

--- frontend/src/components/payment.jsx
+++ frontend/src/components/payment.jsx
@@ -23,18 +23,34 @@ export default function Payment() {
         }
 
     }
-    // Implement payment logic here
-    return (
-        <div className="paymentcontainer" >
-            <div >
-                <form className="form" >
-                    <input type="number" onChange={(e) => { setpayment(e.target.value) }} />
-                    <button type="submit" onClick={addtransaction} > "weeklypayment": {payment}</button>
-                </form>
-            </div>
-        </div >
-    )
-}
+    async function addpaymentS(e) {
+        e.preventDefault()
+        try {
+            console.log(token)
+            const response = await axios.post("https://savings-hndc.onrender.com/api/transactions/pay", { amount: Number(payment) }, {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            if (response.status === 200) {
+                alert("Payment processed successfully with Stripe!");
+            }
+        } catch (err) {
+            alert("retry", err)
+        }
+
+
+        // Implement payment logic here
+        return (
+            <div className="paymentcontainer" >
+                <div >
+                    <form className="form" >
+                        <input type="number" onChange={(e) => { setpayment(e.target.value) }} />
+                        <button type="submit" onClick={addtransaction} > "weeklypayment": {payment}</button>
+                        <button type="submit" onClick={addpaymentS} > "weeklypayment": {payment}</button>
+                    </form>
+                </div>
+            </div >
+        )
+    }
 
 
 
