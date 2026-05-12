dd53f2d9e0c1c06e9bfd98a2eafdad5cf2172d8a
Sarang <sarang.p1230@gmail.com>
2026-05-12 17:08:14 +0530

    error in accepting

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -15,18 +15,13 @@ const express = require("express");
 const router = express.Router();
 const usercontroller = require("../controllers/user-controller");
 const { userMiddleware, multermiddleware, adminpriority } = require("../Middleware/UserMiddleWare");
-
-
-
-
-
 router.post("/register", multermiddleware, usercontroller.register);
 router.post("/login", usercontroller.login);
 router.put("/edituser/:id", adminpriority, usercontroller.edituser)
 // profile routes require authentication
 router.get("/profile/:id", usercontroller.getProfile);
 router.get("/profile", userMiddleware, usercontroller.testgetProfile);
-router.put("/creators", userMiddleware, usercontroller.approveUser)
+router.put("/creators", adminpriority, usercontroller.approveUser)
 
 router.get("/getallusers", usercontroller.getAllUsers);
 module.exports = router;
